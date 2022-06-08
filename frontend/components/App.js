dc1cf3e6ce8f81a29e88422178de7b57b4992ab6
Matthew Arofin <Matt.arofin@gmail.com>
2022-06-07 20:32:59 -0400

    Builds in state and displays static list

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from 'react';
+import TodoList from './TodoList';
+import Form from './Form';
 
-const URL = 'http://localhost:9000/api/todos'
+const URL = 'http://localhost:9000/api/todos';
+
+
+// Hold all todo items in state here
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      todos: [
+        {
+          task: 'Go Shopping',
+          id: 32490,
+          completed: false
+        },
+        {
+          task: 'Do Laundry',
+          id: 29457,
+          completed: false
+        },
+        {
+          task: 'Walk The Dog',
+          id: 73947,
+          completed: false
+        }
+      ]
+    }
+  }
+
+  componentDidMount(){
+    console.log("Component Mounted")
+  }
+
   render() {
-    return null
+    const { todos } = this.state;
+    console.log(todos)
+    return (
+      <div>
+        <TodoList list={this.state.todoList}/>
+        <ul>
+        {
+          todos.map(todo => {
+            return (<li key={todo.id}>{todo.task} {todo.completed ? '✔️' : ''}</li>)
+          })
+        }
+        </ul>
+        <Form />
+      </div>
+    )
   }
 }
